2f0cdbd0a8bb60fba928007cb7ed9960b59973a3
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-06 21:49:59 +0800

    Update Compose_Number_Game.js
    Add the function of changing the state of selected numbers

--- Compose_Number_Game/Compose_Number_Game.js
+++ Compose_Number_Game/Compose_Number_Game.js
@@ -26,18 +26,25 @@ const Button = (props) => {
 const Answer = (props) => {
   return (
     <div className="col-5">
-      <span>5</span>
-      <span>6</span>
+      {props.selectedNumbers.map((number, i) => 
+        <span key={i}>{number}</span>
+      )}
     </div>
   );
 };
 
 const Numbers = (props) => {
+    const numberClassName = (number)=> {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+        return "selected"; 
+    }
+  }
+  
     return (
    <div className="card text-center">
       <div>
         {Numbers.list.map((number, i) =>
-            <span key={i}>{number}</span>
+            <span key={i} className={numberClassName(number)}>{number}</span>
         )}
       </div>
     </div>
@@ -58,10 +65,10 @@ class Game extends React.Component {
         <div className="row">
           <Stars />
           <Button />
-          <Answer />
+          <Answer selectedNumbers={this.state.selectedNumbers} />
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers={this.state.selectedNumbers}/>
       </div>
     );
   }
