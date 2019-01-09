9599249effda9d9534d2cf50f8d29c95485c978c
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-01-09 23:19:09 +0800

    Update Compose_Number_Game.js
    Add answer check function

--- Compose_Number_Game/Compose_Number_Game.js
+++ Compose_Number_Game/Compose_Number_Game.js
@@ -14,9 +14,32 @@
 };
 
 const Button = (props) => {
+    let button;
+  switch(props.answerIsCorrect) {
+    case true:
+        button = 
+        <button className="btn btn-success">
+          <i className="fa fa-check"></i>
+        </button>;
+        break;
+    case false:
+        button = 
+        <button className="btn btn-danger">
+          <i className="fa fa-times"></i>
+        </button>;
+        break;
+    default:
+        button =
+        <button className="btn"
+                        onClick={props.checkAnswer}
+                disabled={props.selectedNumbers.length === 0}>
+            =
+        </button>;
+        break;
+  }
   return (
     <div className="col-2"> {/* 2 cells for a column */}
-       <button className="btn" disabled={props.selectedNumbers.length === 0}>=</button>
+        {button}
     </div>
   );
 };
@@ -56,6 +79,7 @@ class Game extends React.Component {
   state = {
     selectedNumbers: [],
     randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+    answerIsCorrect: null, 
   };
   selectNumber = (clickedNumber) => {
     if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
@@ -68,15 +92,27 @@ class Game extends React.Component {
         selectedNumbers: prevState.selectedNumbers.filter(number => number != clickedNumber)
     }));
   };
+  checkAnswer = () => {
+    this.setState(prevState => ({
+      answerIsCorrect: prevState.randomNumberOfStars ===
+        prevState.selectedNumbers.reduce((acc, n) => acc + n, 0)
+    }));
+    };
   render() {
-    const { selectedNumbers, randomNumberOfStars } = this.state;
+    const { 
+        selectedNumbers,
+      randomNumberOfStars,
+      answerIsCorrect
+    } = this.state;
     return (
       <div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
           <Stars numberOfStars={randomNumberOfStars}/>
-          <Button selectedNumbers={selectedNumbers} />
+          <Button selectedNumbers={selectedNumbers}
+                        checkAnswer={this.checkAnswer}
+                  answerIsCorrect={answerIsCorrect} />
           <Answer selectedNumbers={selectedNumbers}
                         unselectedNumber={this.unselectedNumber} />
         </div>
