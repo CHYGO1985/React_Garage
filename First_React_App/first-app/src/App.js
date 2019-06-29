990f34c7d201b14e2a7c5bb65493958d2b73ee01
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-06-29 13:09:40 +0800

    First react app, add a list

---
+++ First_React_App/first-app/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      {list.map(item => {
+        return (
+          <div> 
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div> );
+          })}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React JJ
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
