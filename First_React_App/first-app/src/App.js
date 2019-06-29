6888a85d04c5da5325d86d71e86fae24e31e2cb1
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-06-29 12:25:21 +0800

    First React App - Add a list and a extra header for list

---
+++ First_React_App/first-app/src/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const items = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {
+          items.map(item => {
+            return (
+              <div key={item.objectID}>
+                <span>
+                  <a href={item.url}>{item.title}</a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+            );
+          })
+        }
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React hhaaa
+        </a>
+      </header>
+      <header className="App-test">
+      {
+        items.map(item => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })
+      }
+      </header>
+    </div>
+  );
+}
+
+export default App;
