8da8e4626de442e722ed2d250fa421762a075625
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-07-12 23:20:26 +0800

    Refactoring Search component and constants

---
+++ First_React_App/first-app/src/components/Search/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Search = ({ searchTerm, onSearchChange, onSubmit, children }) => 
+  <form onSubmit={onSubmit}>
+    <input 
+      type="text"
+      value={searchTerm}
+      onChange={onSearchChange}
+    />
+    <button type="submit">
+      {children}
+    </button>
+  </form>
+  
+export default Search;
