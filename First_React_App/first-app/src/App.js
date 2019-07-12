36cf630b4338e3582753d3b94e440cd08db3a9b2
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-07-12 22:59:48 +0800

    Refactoring contants in the app

--- First_React_App/first-app/src/App.js
+++ First_React_App/first-app/src/App.js
@@ -3,18 +3,19 @@ import axios from 'axios';
 import logo from './logo.svg';
 import React, {Component} from 'react';
 
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP,
+} from './constants'
+
 /**
  * @jingejiejiang updated on Jul 10, 2019
  */
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_HPP = '20';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-// const PATH_BASE = 'https://hn.algolia111.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
 
 // const list = [
 //   {
