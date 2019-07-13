453c218278bf92a81b8858ee50c13691d591ddf8
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-07-13 18:08:13 +0800

    Add test for Search componnet

---
+++ First_React_App/first-app/src/components/Search/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Search from './index';
+import { idText } from 'typescript';
+
+describe('Search', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
