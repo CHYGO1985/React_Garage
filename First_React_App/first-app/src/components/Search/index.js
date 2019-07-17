110b5d941d666055577e5a6b4118d012fcd9081c
Jingjie_Jiang <jingjiejiang85@gmail.com>
2019-07-18 07:32:11 +0800

    Add input focus method

--- First_React_App/first-app/src/components/Search/index.js
+++ First_React_App/first-app/src/components/Search/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const Search = ({ searchTerm, onSearchChange, onSubmit, children }) => 
-  <form onSubmit={onSubmit}>
-    <input 
-      type="text"
-      value={searchTerm}
-      onChange={onSearchChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>
+// const Search1 = ({ searchTerm, onSearchChange, onSubmit, children }) => 
+//   <form onSubmit={onSubmit}>
+//     <input 
+//       type="text"
+//       value={searchTerm}
+//       onChange={onSearchChange}
+//     />
+//     <button type="submit">
+//       {children}
+//     </button>
+//   </form>
+
+class Search extends Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  render() {
+    const {
+      searchTerm,
+      onSearchChange,
+      onSubmit,
+      children
+    } = this.props;
+
+    return (
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text"
+          value={searchTerm}
+          onChange={onSearchChange}
+          ref={(node) => { this.input = node; }}
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   searchTerm: PropTypes.string.isRequired,
