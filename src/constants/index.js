415968a928c99fe6f52d59be24804aab2820d0fc
CHYGO1985 <jingjiejiang85@gmail.com>
2022-07-12 16:06:38 +0800

    delete unused files

--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_QUERY = 'redux';
-export const DEFAULT_HPP = '20';
-
-export const PATH_BASE = 'https://hn.algolia.com/api/v1';
-// const PATH_BASE = 'https://hn.algolia111.com/api/v1';
-export const PATH_SEARCH = '/search';
-export const PARAM_SEARCH = 'query=';
-export const PARAM_PAGE = 'page=';
-export const PARAM_HPP = 'hitsPerPage=';
